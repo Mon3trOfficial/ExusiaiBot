c836081230e6b8f67c28009fc114bfc9583c0974
Mon3tr <Mon3trOfficial@qq.com>
2025-07-25 16:51:01 +0800

    add

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,20 @@
+const raw_parser = require('./module/raw_parser.module')
+
 export default {
   async fetch(request, env, ctx) {
-    console.log("111");
-    return new Response("Hello World!");
+    let message_json;
+
+    if (req.method === "POST") {
+      let body = "";
+
+      req.on("data", (chunk) => {
+        body += chunk;
+      });
+
+      req.on("end", () => {
+        message_json = JSON.parse(body);
+      });
+    }
+
   },
 };
